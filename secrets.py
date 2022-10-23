726263ed14b7669f306703c734007182005deddf
Salvin Lopes <salvinlopes20@gmail.com>
2022-10-24 00:44:04 +0530

    -

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
 # hello this are the secrets do not alter these 🧑‍🎄
 test = '214235346'
 
-DATABASE_URL = 'postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}'
+#DATABASE_URL = 'postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}'
 PGDATABASE = 'railway'
 PGHOST = 'containers-us-west-103.railway.app'
 PGPASSWORD = 'wQGKNThiRw18Et3F60Ga'
